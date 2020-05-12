16b29a2420d737c5491e68191f92795b7088cb1d
ravikundu <32717330+ravikundu@users.noreply.github.com>
2020-05-12 11:06:29 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+var app_firebase = {};
+(function(){
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyCcFrz4T75edfQ4AuXONAFwpPEGSkLf2bA",
+        authDomain: "web-chat-app-be9f7.firebaseapp.com",
+        databaseURL: "https://web-chat-app-be9f7.firebaseio.com",
+        projectId: "web-chat-app-be9f7",
+        storageBucket: "web-chat-app-be9f7.appspot.com",
+        messagingSenderId: "1084555851316",
+        appId: "1:1084555851316:web:4e21c35cb158330cb93c61",
+        measurementId: "G-YZMTK5PJMZ"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    app_firebase = firebase;
+})()
